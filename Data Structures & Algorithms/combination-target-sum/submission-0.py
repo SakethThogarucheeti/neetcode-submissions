50dab46150b2ebade9be683e679dad3947175aab
Saketh Thogarucheeti <47861102+SakethThogarucheeti@users.noreply.github.com>
2026-06-08 17:30:52 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+
+        def dfs(i, subset, summ, freq):
+            
+            if summ == target:
+                if res and res[-1][1] == freq:
+                    return
+                res.append((subset, freq))
+            if i == len(nums):            
+                return
+            
+            curr = subset.copy()
+            currsum = summ
+            currfreq = freq.copy()
+            while currsum <= target:
+                dfs(i+1,  curr, currsum, currfreq)
+                curr = curr + [nums[i]]
+                currsum += nums[i]
+                freq[nums[i]] += 1
+        
+        dfs(0, [], 0, defaultdict(int))
+
+        op = []
+        for subset, freq in res:
+            op.append(subset)
+        return op
+        
