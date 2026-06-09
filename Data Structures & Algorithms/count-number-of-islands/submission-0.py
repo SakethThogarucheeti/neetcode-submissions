b8b7944528140936f9ffaabc2e3fda371d0f420d
Saketh Thogarucheeti <47861102+SakethThogarucheeti@users.noreply.github.com>
2026-06-09 19:18:37 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        rows = len(grid)
+        cols = len(grid[0])
+        dirs = [[1,0], [-1, 0], [0, -1], [0, 1]]
+
+        res = 0
+
+        def dfs(i,j):
+            nonlocal visited, rows, cols, dirs
+
+            if (i,j) in visited:
+                return
+            
+            if 0 <= i < rows and 0 <= j < cols and grid[i][j] == "0":
+                return
+            
+            if 0 <= i < rows and 0 <= j < cols and grid[i][j] == "1":
+                visited.add((i,j))
+                for d in dirs:
+                    dfs(i + d[0], j + d[1])
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    res += 1
+                    dfs(i,j)
+        
+        return res
+
+
+
+
+            
+
+
+        
