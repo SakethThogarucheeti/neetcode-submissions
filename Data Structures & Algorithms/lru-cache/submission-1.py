387a75d5c02e736840693a68e010965555177045
Saketh Thogarucheeti <47861102+SakethThogarucheeti@users.noreply.github.com>
2026-06-17 14:09:18 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,54 @@
+
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev, self.next = None, None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = dict()
+
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+    
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = node
+        nxt.prev = node
+        node.next = nxt
+        node.prev = prev
+        
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insert(node)
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            self.cache.pop(lru.key)
+
+        
