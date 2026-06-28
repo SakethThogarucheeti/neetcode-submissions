cfdbda948ee20a294530337289ba057f91e57c0d
Saketh Thogarucheeti <47861102+SakethThogarucheeti@users.noreply.github.com>
2026-06-28 17:38:29 +0530

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        if len(t) > len(s):
+            return 0
+        lt = len(t)
+        ls = len(s)
+
+        dp = dict()
+
+        def dfs(i, j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if j == lt:
+                return 1
+            if i == ls:
+                return 0
+            
+            res = 0
+            res += dfs(i + 1, j)
+
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            
+            dp[(i,j)] = res
+            return res
+
+        return dfs(0,0)
+                
+        
