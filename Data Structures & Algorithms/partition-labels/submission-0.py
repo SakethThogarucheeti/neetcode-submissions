216cee0d4fddd1f8250b7c42d63892d32885ab37
Saketh Thogarucheeti <47861102+SakethThogarucheeti@users.noreply.github.com>
2026-06-30 22:00:59 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        pos = defaultdict(lambda: (float("inf"), float("-inf")))
+
+        for i in range(len(s)):
+            c = s[i]
+            mini, maxi = pos[c]
+            mini = min(i, mini)
+            maxi = max(i, maxi)
+            pos[c] = (mini, maxi)
+        
+        intervals = list(pos.values())
+        intervals.sort()
+        print(intervals)
+        merged = []
+        for i in range(len(intervals)):
+            s, e = intervals[i]
+            if not merged:
+                merged.append([s,e])
+                continue
+            print(merged)
+            pe = merged[-1][1]
+            print(pe)
+            if pe > s:
+                ps, pe = merged.pop()
+                ne = max(pe, e)
+                merged.append([ps, ne])
+            else:
+                merged.append([s,e])
+        
+        res = []
+
+        for s, e in merged:
+            length = e - s + 1
+            res.append(length)
+        
+        
+        return res
+
+
+
+
+        
